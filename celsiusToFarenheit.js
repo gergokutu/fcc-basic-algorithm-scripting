8322aef17df6c15e1e7479610389c5efc1969e73
gergokutu <gergo.kutu@gmail.com>
2020-11-21 12:39:51 +0100

    Implement celsius to fahrenheit converter

--- celsiusToFarenheit.js
+++ celsiusToFarenheit.js
@@ -1,2 +1,11 @@
 // Convert Celsius to Fahrenheit
 // F = C * 9/5 + 32
+
+// const convertToF = celsius => {
+//   let fahrenheit = celsius * 9 / 5 + 32;
+//   return fahrenheit;
+// }
+
+const convertToF = celsius => celsius * 9 / 5 + 32;
+
+console.log('Should be 86 >', convertToF(30));
