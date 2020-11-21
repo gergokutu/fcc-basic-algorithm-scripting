edc0ac2d08f24bb05d588a81502d267b04bc8ad4
gergokutu <gergo.kutu@gmail.com>
2020-11-21 12:56:41 +0100

    Implement factorialize with for loop

--- factorialize.js
+++ factorialize.js
@@ -1,7 +1,15 @@
-const factorialize = num => {
+const factorializeRec = num => {
   return num <= 1 ?
     1 :
-    num * factorialize(num - 1);
+    num * factorializeRec(num - 1);
 }
+console.log('Should be 120 >', factorializeRec(5));
 
-console.log('Should be 120 >', factorialize(5));
+const factorializeFor = num => {
+  let result = 1;
+  for (let i = 1; i < num + 1; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log('Should be 120 >', factorializeFor(5));
