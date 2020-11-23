3921da198c0b0e43a3afe8ae7e6fe1388247a169
gergokutu <gergo.kutu@gmail.com>
2020-11-23 11:37:13 +0100

    Implement confirmEnd with regex

--- confirmEnd.js
+++ confirmEnd.js
@@ -1,11 +1,22 @@
-// My 1st - endWith()
+// My 1st - endsWith()
 // const confirmEnding = (str, target) => str.endsWith(target);
 
 // My 2nd - slice()
+// const confirmEnding = (str, target) => {
+//   return str.slice(-target.length) === target
+// };
+
+// Alt1 - slice() 2
+// const confirmEnding = (str, target) => {
+//   return str.slice(str.length - target.length) === target
+// };
+
+// My 3rd - regex
 const confirmEnding = (str, target) => {
-  return str.slice([-target.length]) === target ?
-    true :
-    false;
+  const myRegex = new RegExp(target + "$", "i");
+  return myRegex.test(str);
+
+  // return new RegExp(target + "$", "i").test(str)
 };
 
 console.log("Should be true >", confirmEnding("Bastian", "n"));
