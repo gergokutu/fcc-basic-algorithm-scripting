394d58461053e6a3fcd313f2a10b1fecc6b0a1f8
gergokutu <gergo.kutu@gmail.com>
2020-11-23 11:53:17 +0100

    Implement truncateString without slice() method

--- truncateString.js
+++ truncateString.js
@@ -1,8 +1,24 @@
+// My 1st - slice()
+// const truncateString = (str, num) => {
+//   return str.length > num ?
+//     str.slice(0, num) + "..." :
+//     str;
+// };
+
+// My 2nd - without slice() method
 const truncateString = (str, num) => {
-  return str.length > num ?
-    str.slice(0, num) + "..." :
-    str;
-}
+  if (str.length > num) {
+    const stringStart = [];
+
+    for (let i = 0; i < num; i++) {
+      stringStart.push(str[i]);
+    };
+
+    return stringStart.join("") + "...";
+  } else {
+    return str;
+  };
+};
 
 console.log("Should be A-tisket... >",
   truncateString("A-tisket a-tasket A green and yellow basket", 8)
